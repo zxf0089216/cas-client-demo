5cb6e96bad13f436aa4726b4e6a0332796be1451
zxf0089216 <yhh07777@163.com>
2017-03-13 15:08:52 +0800

    1.加入AcmAuthenticationHandler 2.cas-server从https换成http

---
+++ cas-server-support-jdbc/src/main/java/com/hundsun/arescloud/see/cas/AcmAuthenticationHandler.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.hundsun.arescloud.see.cas;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang.StringUtils;
+import org.jasig.cas.adaptors.jdbc.AbstractJdbcUsernamePasswordAuthenticationHandler;
+import org.jasig.cas.authentication.handler.AuthenticationException;
+import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
+
+/**
+ * acm用户名密码认证处理器.
+ *
+ * <p>
+ *     密码加密方式:1.MD5加密 2.sha512加密,两者满足一种就可以
+ * </p>
+ *
+ * @author zhangxr
+ * @version 1.0
+ */
+public class AcmAuthenticationHandler extends AbstractJdbcUsernamePasswordAuthenticationHandler {
+
+    protected final boolean authenticateUsernamePasswordInternal(final UsernamePasswordCredentials credentials) throws AuthenticationException {
+        final String username = getPrincipalNameTransformer().transform(credentials.getUsername());
+        final String password = credentials.getPassword();
+
+        if (StringUtils.isBlank(username)||StringUtils.isBlank(password)){
+            log.warn("username or password is null");
+            return false;
+        }
+
+        final String dbPassword;
+        try{
+            dbPassword = getJdbcTemplate().queryForObject("select auth_password from tb_user where login_name = ?", String.class, username);
+            if (dbPassword==null){
+                return false;
+            }
+        }catch (org.springframework.dao.EmptyResultDataAccessException e){
+            log.info("username not found,username【{}】",username);
+            return false;
+        }
+
+        return dbPassword.equalsIgnoreCase(DigestUtils.sha512Hex(password))
+                || dbPassword.equalsIgnoreCase(DigestUtils.md5Hex(password));
+
+    }
+}
--- cas-server-webapp/README.md
+++ cas-server-webapp/README.md
@@ -18,3 +18,38 @@ keytool -list -storepass changeit -keystore "%JAVA_HOME%\jre\lib\security\cacert
 keytool -delete -alias cas-server -storepass changeit
 
 keytool -delete -trustcacerts -alias cas-server -keystore "C:\Program Files\Java\jdk1.7.0_51\jre\lib\security\cacerts" -storepass changeit
+
+
+create table t_admin_user (
+    id bigint not null auto_increment,
+    email varchar(255),
+    login_name varchar(255) not null unique,
+    name varchar(255),
+    password varchar(255),
+    primary key (id)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE `tb_user` (
+  `id` VARCHAR(32) NOT NULL,
+  `login_name` VARCHAR(64) DEFAULT NULL,
+  `real_name` VARCHAR(64) DEFAULT NULL,
+  `lock_flag` VARCHAR(8) DEFAULT NULL COMMENT '0-正常\r\n            1-注销',
+  `mobilephone` VARCHAR(32) DEFAULT NULL,
+  `email` VARCHAR(128) DEFAULT NULL,
+  `role_id` VARCHAR(4000) DEFAULT NULL,
+  `env_id` VARCHAR(64) DEFAULT NULL,
+  `remark` VARCHAR(256) DEFAULT NULL,
+  `creator` VARCHAR(64) DEFAULT NULL,
+  `ext_field` VARCHAR(128) DEFAULT NULL,
+  `last_modified_by` VARCHAR(225) DEFAULT NULL,
+  `last_modified` DATETIME DEFAULT NULL,
+  `auth_password` VARCHAR(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+
+INSERT  INTO `tb_user`(`id`,`login_name`,`real_name`,`lock_flag`,`mobilephone`,`email`,`role_id`,`env_id`,`remark`,`creator`,`ext_field`,`last_modified_by`,`last_modified`,`auth_password`) VALUES 
+('1','admin',NULL,NULL,NULL,NULL,'1','001',NULL,NULL,NULL,NULL,NULL,'c4ca4238a0b923820dcc509a6f75849b');
+
+
